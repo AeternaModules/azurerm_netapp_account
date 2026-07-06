4191e10c25d91993d3c7763d2dbf9eb703e8da47
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:13:57 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,31 @@
 output "netapp_accounts" {
   description = "All netapp_account resources"
   value       = azurerm_netapp_account.netapp_accounts
+  sensitive   = true
+}
+output "netapp_accounts_active_directory" {
+  description = "List of active_directory values across all netapp_accounts"
+  value       = [for k, v in azurerm_netapp_account.netapp_accounts : v.active_directory]
+  sensitive   = true
+}
+output "netapp_accounts_identity" {
+  description = "List of identity values across all netapp_accounts"
+  value       = [for k, v in azurerm_netapp_account.netapp_accounts : v.identity]
+}
+output "netapp_accounts_location" {
+  description = "List of location values across all netapp_accounts"
+  value       = [for k, v in azurerm_netapp_account.netapp_accounts : v.location]
+}
+output "netapp_accounts_name" {
+  description = "List of name values across all netapp_accounts"
+  value       = [for k, v in azurerm_netapp_account.netapp_accounts : v.name]
+}
+output "netapp_accounts_resource_group_name" {
+  description = "List of resource_group_name values across all netapp_accounts"
+  value       = [for k, v in azurerm_netapp_account.netapp_accounts : v.resource_group_name]
+}
+output "netapp_accounts_tags" {
+  description = "List of tags values across all netapp_accounts"
+  value       = [for k, v in azurerm_netapp_account.netapp_accounts : v.tags]
 }
 
